17830b688d354fe9a252cf98130ecc349c1e28b8
laminazaman <84242212+laminazaman@users.noreply.github.com>
2024-02-28 18:18:56 -0500

    Update 2rado.py

--- 2rado.py
+++ 2rado.py
@@ -1,9 +1,9 @@
 from pysat.solvers import Solver
 
-k = 2 #colours
-n = 2 #start
+k = 2 # colours
+n = 2 # start
 
-#eqn: ax + by = cz
+# eqn: ax + by = cz
 
 a = int(input())
 b = int(input())
@@ -18,9 +18,9 @@ with Solver() as s:
 
         for x in range(1, n + 1):
             for y in range(1, n + 1):
-                if a*x + b*y <= c*n and (a*x + b*y) % c == 0:
-                    clause = [x, y, (a*x + b*y)//c]
-                    print([i for i in clause])
+                z = (a*x + b*y) // c
+                if a*x + b*y == c*z and 1 <= z and z <= n:
+                    clause = [x, y, z]
                     s.add_clause(clause)
                     s.add_clause([-i for i in clause])
 
